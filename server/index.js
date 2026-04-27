52fb814f60872b897e4f3521c9d08934c1b447e6
David <oforkansidavid45@gmail.com>
2026-04-27 02:56:35 -0700

    fix render port

--- server/index.js
+++ server/index.js
@@ -45,6 +45,6 @@ io.on("connection", (socket) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
