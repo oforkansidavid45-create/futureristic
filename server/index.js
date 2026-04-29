4579ca4dfdfde853d815f5cf3415d1ff45f2dded
David <oforkansidavid45@gmail.com>
2026-04-29 01:07:48 -0700

    message

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Facebook Clone API Running 🚀");
+  res.sendFile(__dirname + "/../client/index.html");
 });
 
 // DB CONNECT
